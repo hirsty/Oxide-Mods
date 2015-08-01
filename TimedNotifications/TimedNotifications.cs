191b84107ce5c57c6fd170bd3bd5350e34b22c0d
Daniel Hirst <dhirst175@hotmail.com>
2015-08-01 12:09:11 +0100

    Took Event Checker away from OnTick Hok and placed on configurable timer. Added storage engine for events, Popup works. Timed notifications will fire if missed

--- TimedNotifications/TimedNotifications.cs
+++ TimedNotifications/TimedNotifications.cs
@@ -2,6 +2,7 @@
 using Oxide.Core.Plugins;
 using Oxide.Game.Rust.Libraries;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Reflection;
 using System;
 using System.Linq;
@@ -22,50 +23,47 @@ namespace Oxide.Plugins
         public string[] days;
         public string TZ;
         public int hour;
+        public DateTime setDate;
         public int min;
+        char sep = '/';
+        char sep2 = ':';
 
-        [PluginReference]
+[PluginReference]
         Plugin PopupNotifications;
-        class EventData
-        {
-            public EventData()
-            {
-               
-            }
-        }
+        
         class StoredData
         {
-            public HashSet<PlayerInfo> Players = new HashSet<PlayerInfo>();
+            public HashSet<EventData> Events = new HashSet<EventData>();
 
             public StoredData()
             {
             }
         }
-
-        class PlayerInfo
+        class EventData
         {
-            public string UserId;
-            public string Name;
-
-            public PlayerInfo()
+            public DateTime EventDate;
+            public string EventInfo;
+            public bool broadcast;
+            public EventData()
             {
+
             }
-
-            public PlayerInfo(BasePlayer player)
+            public EventData(DateTime Date, string info)
             {
-                UserId = player.userID.ToString();
-                Name = player.displayName;
-                
+               this.EventDate = Date;
+               this.EventInfo = info;
+                this.broadcast = false;
             }
         }
-        EventData eventData;
+        StoredData storedData;
         protected override void LoadDefaultConfig() {
             PrintWarning("Whoops! No config file, lets create a new one!"); // Runs when no configuration file has been found
             Config.Clear();
             Config["Plugin","Version"] = version;
             //Config["Events", "TimeZone"] = "GMT";
             Config["Events", "AllowedDays"] = "Mon,Tue,Wed,Thu,Fri,Sat,Sun";
-            Config["Plugin", "PopUpTime"] = "20";
+            Config["Plugin", "PopUpTime"] = 20;
+            Config["Plugin", "TimeCheck"] = 10;
             Config["Plugin", "EnablePopUps"]=true;
             SaveConfig();
         }
@@ -85,14 +83,26 @@ namespace Oxide.Plugins
                     PopUpNotifier = (bool)Config["Plugin", "EnablePopups"];
                 }
             }
+
+
+            //PopupNotifications.Call("CreatePopupNotification", "Test message",null,10);
             string[] seperator = new string[] { "," };
             // Build array of events
+            int tick;
+            if (Config["Plugin", "TimeCheck"] != null)
+            {
+                tick = (int)Config["Plugin", "TimeCheck"];
+            } else
+            {
+                tick = 10;
+            }
             string daystring = (string)Config["Events", "AllowedDays"];
             days = daystring.Split(seperator, StringSplitOptions.RemoveEmptyEntries);
             //Puts(days.GetValue(0).ToString());
             //PopupNotifications.Call("CreatePopupNotification", "Test message");
-            eventData = Interface.GetMod().DataFileSystem.ReadObject<EventData>("MyEvents");
-           
+            storedData = Interface.GetMod().DataFileSystem.ReadObject <StoredData>("MyEvents");
+            timer.Repeat(tick, 0, () => EventCheck());
+
         }
 
         [ChatCommand("notification")] // Whatever cammand you want the player to type
@@ -104,13 +114,16 @@ namespace Oxide.Plugins
                     SendHelp(player);
                     break;
                 case 1:
-                    switch (args[1])
+                    switch (args[0])
                     {
                         case "list":
                             // List Notifications
                             break;
                         case "reset":
                             // Remove all notifications
+                            storedData.Events.Clear();
+                            Interface.GetMod().DataFileSystem.WriteObject("MyEvents", storedData);
+                            SendChatMessage(player, "TimeNotify:", "Events have been removed!");
                             break;
                         default:
                             SendHelp(player);
@@ -118,10 +131,26 @@ namespace Oxide.Plugins
                     }
                     break;
                 case 2:
-                    switch (args[1])
+                case 3:
+                case 4:
+                    switch (args[0])
                     {
                         case "add":
-                            // Add a Notification
+                            string[] datepart = args[1].Split(sep);
+                            string[] timepart = args[2].Split(sep2);
+                            try
+                            {
+                                setDate = new DateTime(datepart[2].ToInt(), datepart[1].ToInt(), datepart[0].ToInt(), timepart[0].ToInt(), timepart[1].ToInt(), 0);
+                            } catch
+                            {
+                                SendHelp(player);
+                            }
+                            
+                            string eventinfo = args[3];
+                            var info = new EventData(setDate,eventinfo);
+                            storedData.Events.Add(info);
+                            SendChatMessage(player, "TimedNotify", "Event Saved");
+                            Interface.GetMod().DataFileSystem.WriteObject("MyEvents", storedData);
                             break;
                         case "remove":
                             // Remove a Notification]
@@ -139,15 +168,37 @@ namespace Oxide.Plugins
             // Called when server is booted up
         }
 
-        void OnTick()
+        void EventCheck()
         {
             if (!days.Contains(DateTime.Now.ToUniversalTime().ToString("ddd")))
             {
                 return;
             } else {
+                foreach(var storedEvent in storedData.Events)
+                {
+                    if(storedEvent.EventDate.Date <= DateTime.UtcNow.Date && !storedEvent.broadcast)
+                    {
+                        SendNotification(storedEvent.EventInfo, (int)Config["Plugin", "PopUpTime"]);
+                        storedEvent.broadcast = true;
+                        Interface.GetMod().DataFileSystem.WriteObject("MyEvents", storedData);
+                    }
+
+                    if (storedEvent.EventDate.Date <= DateTime.UtcNow.Date && !storedEvent.broadcast)
+                    {
+                        if (storedEvent.EventDate.Hour <= DateTime.UtcNow.Hour && !storedEvent.broadcast)
+                        {
+                            if (storedEvent.EventDate.Minute <= DateTime.UtcNow.Minute && !storedEvent.broadcast)
+                            {
+                                SendNotification(storedEvent.EventInfo, (int)Config["Plugin", "PopUpTime"]);
+                                storedEvent.broadcast = true;
+                                Interface.GetMod().DataFileSystem.WriteObject("MyEvents", storedData);
+                            }
+                        }
+                    }
+                }
+                // Check for Events on that day
 
             }
-            // Do something  on server "Tick" - defined by tickrate
         }
 
         void OnPluginUnloaded()
@@ -156,27 +207,28 @@ namespace Oxide.Plugins
         }
         void SendHelp(BasePlayer player)
         {
-            SendChatMessage(player, "TimedNotify", "Help Text");
-            SendChatMessage(player, "TimedNotify", "Help Text");
-            SendChatMessage(player, "TimedNotify", "Help Text");
-            SendChatMessage(player, "TimedNotify", "Help Text");
-            SendChatMessage(player, "TimedNotify", "Help Text");
-            SendChatMessage(player, "TimedNotify", "Help Text");
-            SendChatMessage(player, "TimedNotify", "Help Text");
-            SendChatMessage(player, "TimedNotify", "Help Text");
+            SendChatMessage(player, "TimedNotify", "/notification add <DD/MM/YY> <HH:MM> \"<MESSAGE>\". To schedule a notification for the specified time");
         }
 
         //---------------------------->   Chat Sending   <----------------------------//
 
-        void SendNotification(string message, int delay)
+        void SendNotification(string message, int delay=5,BasePlayer player=null)
         {
             if (PopUpNotifier)
             {
-                PopupNotifications.Call("CreatePopupNotification", message,null, delay);
+                
+                PopupNotifications.Call("CreatePopupNotification", "<color=orange>Notification:</color> " + message, player, delay);
+               
             }
             else
             {
-                BroadcastChat("Notification", message);
+                if (player != null)
+                {
+                    SendChatMessage(player, "Notification", message);
+                } else
+                {
+                    BroadcastChat("Notification", message);
+                }
             }
         }
         void BroadcastChat(string prefix, string msg)
